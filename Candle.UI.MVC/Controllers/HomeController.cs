7eb27a5b2cfc18ea8178e65fd400b6fc8f2c99d4
Kate Baldwin <marykatbaldwin@gmail.com>
2023-12-15 02:54:31 -0600

    added contact page

--- Candle.UI.MVC/Controllers/HomeController.cs
+++ Candle.UI.MVC/Controllers/HomeController.cs
@@ -1,29 +1,78 @@
 ﻿using Candle.UI.MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace Candle.UI.MVC.Controllers
 {
      public class HomeController : Controller
      {
           private readonly ILogger<HomeController> _logger;
+		private readonly IConfiguration _config;
 
-          public HomeController(ILogger<HomeController> logger)
-          {
-               _logger = logger;
-          }
+		public HomeController(ILogger<HomeController> logger, IConfiguration config)
+		{
+			_logger = logger;
+			_config = config;
+		}
 
-          public IActionResult Index()
-          {
-               return View();
-          }
-
-          public IActionResult Privacy()
+		public IActionResult Index()
           {
                return View();
           }
 
-          [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+         
+
+		#region Email
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Contact(ContactViewModel cvm)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(cvm);
+			}
+
+			string message = $"You have received an email from {cvm.Name} (reply to: {cvm.Email}).\n* Subject: {cvm.Subject}\n* Message: \n{cvm.Message}";
+			var mm = new MimeMessage();
+			mm.From.Add(new MailboxAddress("No Reply", _config.GetValue<string>("Credentials:Email:User")));
+			mm.To.Add(new MailboxAddress("You", _config.GetValue<string>("Credentials:Email:Recipient")));
+
+			mm.Subject = cvm.Subject;
+
+			mm.Body = new TextPart("HTML") { Text = message };
+
+			mm.ReplyTo.Add(new MailboxAddress(cvm.Name, cvm.Email));
+
+			using (var client = new SmtpClient())
+			{
+				client.Connect(_config.GetValue<string>("Credentials:Email:Client"));
+
+				client.Authenticate(
+				    _config.GetValue<string>("Credentials:Email:User"),
+				    _config.GetValue<string>("Credentials:Email:Password")
+				    );
+				try
+				{
+					client.Send(mm);
+				}
+				catch (Exception ex)
+				{
+					ViewBag.ErrorMessage = $"There was an error sending the email.  Please try again later..";
+					return View(cvm);
+				}
+			}
+			return View("EmailConfirmation", cvm);
+		}
+
+		#endregion
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
           public IActionResult Error()
           {
                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ Candle.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Candle.UI.MVC.Models
+{
+	public class ContactViewModel
+     {
+		[Required(ErrorMessage = "* Name is required")]
+		public string Name { get; set; } = null!;
+
+		[Required(ErrorMessage = "* Email address is required")]
+		[EmailAddress(ErrorMessage = "* Must be a valid email")]
+		public string Email { get; set; } = null!;
+		[Required(ErrorMessage = "* Subject is required")]
+		public string Subject { get; set; } = null!;
+		[Required(ErrorMessage = "* Message is required")]
+		public string Message { get; set; } = null!;
+	}
+}
